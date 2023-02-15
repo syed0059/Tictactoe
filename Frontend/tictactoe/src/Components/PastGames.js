c207a324cf9273dd1b46dedbb3f4d09aca70d9f5
Saad <syedsaad9@gmail.com>
2023-02-15 22:21:24 +0800

    pastgames

--- Frontend/tictactoe/src/Components/PastGames.js
+++ Frontend/tictactoe/src/Components/PastGames.js
@@ -6,7 +6,7 @@ function PastGames({ past }) {
     for (const game of Object.keys(past)) {
         games.push(
             <div key={game}>
-                <TableRow><TableCell style={{padding:10}}><Typography variant="h3">Game {game}</Typography></TableCell>
+                <TableRow><TableCell style={{padding:10}}><Typography variant="h3">{game}</Typography></TableCell>
                 <TableCell style={{padding:10}}><Typography variant="h4">{past[game].map(el => {
                     return (<Typography variant="h6">{el}</Typography>)
                 })}</Typography></TableCell></TableRow>
